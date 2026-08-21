f9adc8e32292400a5678046b0810a8d6cb8bf5e7
Julian Kuelshammer <julian.kuelshammer@math.uu.se>
2026-08-21 22:19:33 +0200

    reindex Threshold and introduce bracketing

--- HenningWalterCombinatorics/Catalan.lean
+++ HenningWalterCombinatorics/Catalan.lean
@@ -1,58 +1,61 @@
-import Mathlib
+import Mathlib.Combinatorics.Enumerative.Catalan.Basic
+import Mathlib.Data.Int.ConditionallyCompleteOrder
 
-def IsThreshold (n : ℕ) (h : Fin (n + 1) → Fin (n + 2)) : Prop :=
-(h 0 = 0) ∧
-(∀i, h i ≤ n + 1 - i.val) ∧
-(∀i j, i ≤ j → h i ≤ n + 1 - j.val → h i ≤ h j)
+def IsThreshold (n : ℕ) (h : Fin n → Fin n) : Prop :=
+(∀i, h i ≤ n - (i.val + 1)) ∧
+(∀i j, i ≤ j → h i ≤ n - (j.val + 1) → h i ≤ h j)
 
-instance (n : ℕ) (h : Fin (n + 1) → Fin (n + 2)) :
+instance (n : ℕ) (h : Fin n → Fin n) :
     Decidable (IsThreshold n h) := by
   unfold IsThreshold
   infer_instance
 
-def IsYSequence (n : ℕ) (a : Fin (n + 1) → Fin (n + 2)) : Prop :=
-(a 0 = n + 1) ∧
+def IsBracketing (n : ℕ) (a : Fin n → Fin n) : Prop :=
 (∀i, i.val ≤ a i) ∧
 (∀i j, i ≤ j → j.val ≤ a i → a j ≤ a i)
 
-instance (n : ℕ) (h : Fin (n + 1) → Fin (n + 2)) :
-    Decidable (IsYSequence n h) := by
-  unfold IsYSequence
+instance (n : ℕ) (h : Fin n → Fin n) :
+    Decidable (IsBracketing n h) := by
+  unfold IsBracketing
   infer_instance
 
-def ThresholdSequences (n : ℕ) : Finset (Fin (n + 1) → Fin (n + 2)) :=
+def ThresholdSequences (n : ℕ) : Finset (Fin n → Fin n) :=
 Finset.univ.filter (IsThreshold n)
 
-lemma mem_ThresholdSequences {n : ℕ} (h : Fin (n + 1) → Fin (n + 2)):
-h ∈ ThresholdSequences n ↔ (h 0 = 0) ∧ (∀i, h i ≤ n + 1 - i.val) ∧
-(∀i j, i ≤ j → h i ≤ n + 1 - j.val → h i ≤ h j) := by
+lemma mem_ThresholdSequences {n : ℕ} (h : Fin n → Fin n) :
+h ∈ ThresholdSequences n ↔ (∀i, h i ≤ n - (i.val + 1)) ∧
+(∀i j, i ≤ j → h i ≤ n - (j.val + 1) → h i ≤ h j) := by
   simp [ThresholdSequences, IsThreshold]
 
+#eval (ThresholdSequences 0).card
+#eval (ThresholdSequences 1).card
+#eval (ThresholdSequences 2).card
+#eval (ThresholdSequences 3).card
 #eval (ThresholdSequences 4).card
 
-def YSequences (n : ℕ) : Finset (Fin (n + 1) → Fin (n + 2)) :=
-Finset.univ.filter (IsYSequence n)
+def BracketingSequences (n : ℕ) : Finset (Fin n → Fin n) :=
+Finset.univ.filter (IsBracketing n)
 
-lemma mem_YSequences {n : ℕ} (a : Fin (n + 1) → Fin (n + 2)) :
-a ∈ YSequences n ↔ (a 0 = n + 1) ∧ (∀i, i.val ≤ a i) ∧
+lemma mem_BracketingSequences {n : ℕ} (a : Fin n → Fin n) :
+a ∈ BracketingSequences n ↔ (∀i, i.val ≤ a i) ∧
 (∀i j, i ≤ j → j.val ≤ a i → a j ≤ a i) := by
-  simp [YSequences, IsYSequence]
+  simp [BracketingSequences, IsBracketing]
 
-#eval (YSequences 4).card
+#eval (BracketingSequences 0).card
+#eval (BracketingSequences 1).card
+#eval (BracketingSequences 2).card
+#eval (BracketingSequences 3).card
+#eval (BracketingSequences 4).card
 
-def Threshold_equiv_Y (n : ℕ) : ThresholdSequences n ≃ YSequences n :=
+def Threshold_equiv_Bracketing (n : ℕ) : ThresholdSequences n ≃ BracketingSequences n :=
 { toFun := fun h =>
-    ⟨fun i => ⟨n + 1 - h.1 i, by omega⟩, by
+    ⟨fun i => ⟨n - (h.1 i + 1), by omega⟩, by
       rcases h with ⟨h, hh⟩
       simp only [mem_ThresholdSequences] at hh
-      rcases hh with ⟨hh1, hh2, hh3⟩
-      simp only [YSequences, Finset.mem_filter, Finset.mem_univ, true_and]
-      constructor
-      · simp [hh1]
-      · grind⟩,
+      grind [IsBracketing, BracketingSequences]⟩,
   invFun := fun a =>
-    ⟨fun i => ⟨n + 1 - a.1 i, by omega⟩, by
-      grind [mem_YSequences, mem_ThresholdSequences, Fin.mk_eq_zero]⟩,
+    ⟨fun i => ⟨n - (a.1 i + 1), by omega⟩, by
+      grind [mem_BracketingSequences, mem_ThresholdSequences, Fin.mk_eq_zero]⟩,
   left_inv := by
     intro h
     ext i
@@ -61,3 +64,9 @@ def Threshold_equiv_Y (n : ℕ) : ThresholdSequences n ≃ YSequences n :=
     intro a
     ext i
     grind }
+
+theorem bracketingSequences_card (n : ℕ) : (BracketingSequences n).card = catalan n := by
+  induction n using Nat.case_strong_induction_on with
+  | hz => simp [BracketingSequences, IsBracketing]
+  | hi n ih =>
+    sorry
